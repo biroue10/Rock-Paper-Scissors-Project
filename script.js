dee1b790a0af40978c41ab7ed9d7eccb8ce7282d
BIROUE ISAAC <biroueisaac@gmail.com>
2023-06-22 19:59:27 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// We are going to build a function which will randomly return ‘Rock’, ‘Paper’ or ‘Scissors’.
+const tableau = ["rock","paper","scissors"];
+const getComputerChoice = (tableau)=>{
+    const randomIndex = Math.floor(Math.random() * tableau.length);
+    // get random item
+    const item = tableau[randomIndex];
+    return item;
+}
+let answer=""
+const playRound = (playerSelection, computerSelection)=>{
+    if(playerSelection===computerSelection){
+        answer="tie";
+    }
+    else if(playerSelection==="paper" && computerSelection==="rock"){
+      answer="you win and computer lost";
+    }
+    else if(playerSelection==="scissors" && computerSelection==="rock"){
+        answer="you lost and computer win";
+    }
+    else if(playerSelection==="rock" && computerSelection==="paper"){
+        answer="you lost and computer win";
+    }
+    else if(playerSelection==="scissors" && computerSelection==="paper"){
+        answer="you win and computer lost";
+    }
+    else if(playerSelection==="rock" && computerSelection==="scissors"){
+        answer="you win and computer lost";
+    }
+    else if(playerSelection==="paper" && computerSelection==="scissors"){
+        answer="you lost and computer win";
+    }
+    return answer
+}
+let playerSelection = "rock"
+let computerSelection = getComputerChoice(tableau);
+let response = playRound(playerSelection, computerSelection)
+console.log(response)
